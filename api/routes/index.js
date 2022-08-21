94337314d7344e01445d30f37e8a3ea60cef64de
Ramya <ramya.s0311@gmail.com>
2022-08-21 23:51:03 +0800

    created upload function

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 
-// authentication
+// upload
 router.post('/upload', ctrlUpload.upload);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 require('./api/models/db');
-require('./api/config/passport');
 
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
+const bodyParser = require("body-parser");
 const express = require('express');
 const logger = require('morgan');
-const passport = require('passport');
 const path = require('path');
+const formidable = require('express-formidable');
 
 const routesApi = require('./api/routes/index');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(formidable({ multiples: true }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,20 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize());
 app.use("/api", routesApi);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404));
-});
-
-// Catch unauthorised errors
-app.use((err, req, res) => {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401);
-        res.json({ message: `${err.name}: ${err.message}` });
-    }
 });
 
 // error handler
@@ -43,7 +36,6 @@ app.use((err, req, res, next) => {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
 });
 
 module.exports = app;
